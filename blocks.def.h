4178ef92fcb1acf0fc8c8051045bc05de771227d
player01osu <player01osu@gmail.com>
2022-04-29 23:14:45 -0400

    much needed clean up
    commands fully inside scripts now

--- blocks.def.h
+++ blocks.def.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/                                              /*Command*/                                        /*Update Interval*/         /*Update Signal*/
 	{"",                                                  "todonow",                                          0,                        12},
-	{"^c#44475a^^b#81A1C1^  Yuri Folder ",               "yurisize",                                         400,                       0},
+	{"",                                                  "yurisize",                                         400,                       0},
 	{"",                                                  "mstatus",                                          1,                         0},
-	{" ^c#81A1C1^^b#373d49^ ",                           "acpi | awk -F, '{ print $2 }'",                    400,                       0},
-	{" ^c#373d49^^b#7aa2f7^  ^c#282c34^^b#668ee3^ ",     "date '+(%a) %d %b'",                               12000,                     0},
-	{" ^c#373d49^^b#81A1C1^  ",                          "echo ^c#373d49^^b#7292b2^ $(date '+%H:%M') ^d^",   60,                        0},
+	{"",                                                  "bati",                                             400,                       0},
+	{"",                                                  "datebar",                               12000,                     0},
+	{"",                                                  "timebar",   60,                        0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
