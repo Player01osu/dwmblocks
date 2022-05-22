7b1adf5db1d27101bd4f2baa3783bd8f6b241cb3
player01osu <player01osu@gmail.com>
2022-05-22 19:57:32 -0400

    reformat and have music script on listen

--- blocks.def.h
+++ blocks.def.h
@@ -3,10 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/                                              /*Command*/                                        /*Update Interval*/         /*Update Signal*/
 	{"",                                                  "todonow",                                          0,                        12},
 	{"",                                                  "yurisize",                                         400,                       0},
-	{"",                                                  "mstatus",                                          1,                         0},
+	{"",                                                  "mstatus",                                          0,                        11},
 	{"",                                                  "bati",                                             400,                       0},
-	{"",                                                  "datebar",                               12000,                     0},
-	{"",                                                  "timebar",   60,                        0},
+	{"",                                                  "datebar",                                          12000,                     0},
+	{"",                                                  "timebar",                                          60,                        0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
